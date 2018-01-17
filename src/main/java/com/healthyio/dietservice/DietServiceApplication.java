c911906810e7df95693ff5cd0c448e2eff8b8a94
Vishal Hastir <Vishal.Hastir@mpowersoft.in>
2018-01-17 14:08:51 +0530

    diet structure

---
+++ src/main/java/com/healthyio/dietservice/DietServiceApplication.java
@@ -0,0 +1,12 @@
+package com.healthyio.dietservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DietServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DietServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/healthyio/dietservice/DietServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.healthyio.dietservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DietServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
